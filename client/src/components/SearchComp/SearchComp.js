a85b8b701dcac2beb34dc5380192c5e216728b37
Ian Applekamp <ieapplekamp@gmail.com>
2020-01-10 21:08:04 -0500

    searchcomp is now working correctly and cleaned up code for bookscomp

--- client/src/components/SearchComp/SearchComp.js
+++ client/src/components/SearchComp/SearchComp.js
@@ -2,27 +2,22 @@ import React from "react";
 
 function SearchComp(props) {
     return (
-        <nav className="navbar">
-      <div className="navbar-brand">Google Books Search!</div>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav">
-          <li className={`nav-item ${props.Search}`}>
-            <a className="nav-link" href="./Search">Search</a>
-          </li>
-          <li className={`nav-item ${props.Saved}`}>
-            <a className="nav-link" href="./Saved">Saved</a>
-          </li>
-        </ul>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link text-danger" href="" target="_blank" rel="noopener noreferrer">Github Repo</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <div className="container-fluid">
+            <div className="col-lg-12">
+                <div className="card">
+                    <div className="card-body">
+                        <h4>Book Search:</h4>
+                        <form>
+                            <div className="form-group">
+                                <label className="font-italic">Search Title or Author</label>
+                                <input type="text" className="form-control" id="bookInput" name="search" value={props.search} onChange={props.handleInputChange} placeholder="1984"/>
+                            </div>
+                            <button onClick={props.bookSearch} className="btn btn-primary float-right">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
