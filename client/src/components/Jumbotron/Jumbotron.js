6b175fd9b4966ca2595278391e11948e54eb3474
Ian Applekamp <ieapplekamp@gmail.com>
2020-02-21 17:11:15 -0500

    adjusted text size

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ import "./Jumbotron.css"
 function Jumbotron() {
   return (
     <div className="jumbotron">
-        <h1 className="display-3 text-center">Google Books Search</h1>
+        <h1 className="display-4 text-center">Google Books Search</h1>
         <h4 className="text-center mt-4">Search and Save books of interest!</h4>
     </div>
   );
